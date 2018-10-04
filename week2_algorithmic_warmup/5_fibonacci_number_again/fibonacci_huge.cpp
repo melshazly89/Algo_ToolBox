23a1c670a1fb651138219ba9d01c2bebf4541b7d
Mahmoud Elshazly <mahmoud.elshazly@delphi.com>
2018-10-04 16:09:57 +0200

    Fibonnaci_Huge

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
+long long get_fibonacci_huge_fast(long long n, long long m) {
+	 int length=Pisano_Period(m);
+	 int Fn[n];
+	 int sum=0;
+	 int result=0;
 
-long long get_fibonacci_huge_naive(long long n, long long m) {
-	long long result[n];
-	result[0]=0;
-	result[1]=1;
-	for(int i=2;i<=n;i++)
+	for(int i=0;i<=n;i++)
 	{
-		result[i]=(result[i-1]+result[i-2])%m;
+	    if(i>1)
+	    {
+		Fn[i]=(Fn[i-1]+Fn[i-2]);
+	    }
+	    else
+	    {
+	        Fn[i]=i;
+	    }
+	    sum+=Fn[i];
 	}
+		result=(sum%length)%m;
 
-    return result[n];
+
+    return result;
+}
+int Pisano_Period(long long m)
+{
+	int previous=1;
+	int current=1;
+	int result;
+	int buffer;
+	while(previous !=0 && current!=1)
+	{
+		buffer=(previous+current)%m;
+		previous=current;
+		current=buffer;
+		result+=1
+	}
+return result;
 }
 
 int main() {
     long long n, m;
     std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
 }
--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -18,8 +18,33 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int get_fibonacci_last_digit_fast(int n) {
+	int Fn[n];
+	int sum=0;
+	int result=0;
+	Fn[0]=0;
+	Fn[1]=1;
+
+	for(int i=0;i<=n;i++)
+	{
+	    if(i>1)
+	    {
+		Fn[i]=(Fn[i-1]+Fn[i-2]);
+	    }
+	    else
+	    {
+	        Fn[i]=i;
+	    }
+	    sum+=Fn[i];
+	}
+		result=sum%10;
+
+
+    return result;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << get_fibonacci_last_digit_fast(n);
 }
