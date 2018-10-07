95deab358340cfdb685f4fecc54d9c1586b00caf
Melshazly <mahmoud.elshazly89@gmail.com>
2018-10-07 13:04:35 +0200

    Money_Change Assignment

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
 int get_change(int m) {
-  int Div;
-  int Rem;
+  int n=0;
+  while(0!=m)
+  {
+     if(m%10==0)
+     {
+         m=m-10;
+         
+     }
+     else if(m%5==0)
+     {
+        m=m-5;
+     }
+     else
+     {
+         m=m-1;
+     }
+     n++;
+  }
   return n;
 }
 
